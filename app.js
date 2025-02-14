b703d877c649970b7ff875356b18557814bdcd2c
D5avid <3220064979@qq.com>
2025-02-14 10:20:10 +0800

    express

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const router = require("./routes");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(cors()); // 允许跨域
+app.use(morgan("dev"));
+
+app.use("/api/v1", router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
